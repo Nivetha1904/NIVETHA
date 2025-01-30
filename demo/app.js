9607c564996701c3bbe445b857ffc044ef82fe1c
Nivetha1904 <nivethasaravanan09@gmail.com>
2025-01-30 11:59:39 +0530

    mern

---
+++ demo/app.js
@@ -0,0 +1,5 @@
+
+let mark=75;
+let grade=(mark>=90)?"A" : (mark>70)? "B":(mark>=40)?"C"
+:"F";
+console.log(grade);
---
+++ new.js
@@ -0,0 +1,29 @@
+let user={
+    name:"kendrick",
+    age:30,
+    "is singer":true,
+}
+console.log(user);
+for(let key in user){
+    console.log(key+":"+user[key]);
+}
+const array=[1,2,3,6,4,5]
+for(let value of array){
+    console.log(value);
+}
+
+/*let mark=60;
+let grade=(mark>=90)?"A" : (mark>70)? "B":(mark>=40)?"C"
+:"F";
+console.log(grade);
+
+
+
+
+let num=3;
+let n=(num%2==0)? "Even":"odd"
+console.log(n)*/
+
+
+/*let user={name:"user1",age:"20"}
+let address={city:"CBE",country:"India"}*/
